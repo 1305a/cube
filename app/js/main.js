bf266e729bb8c0600164209984cd55278b4b9849
1305a <1305andrew2@gmail.com>
2021-03-26 16:52:40 +0300

    20210326_1652

--- app/js/main.js
+++ app/js/main.js
@@ -102,89 +102,81 @@ rnd.addEventListener ('click', (event) => {
     cube6.style.order = randomArray[5]
 })
 
+//--------------------------------------------------
 
 
-
-/*function moveCube (cube,direction) {
-    const cubeList = []
-    cubeList[0] = {order: parseInt(cube1.style.order), cube: cube1}
-    cubeList[1] = {order: parseInt(cube2.style.order), cube: cube2}
-    cubeList[2] = {order: parseInt(cube3.style.order), cube: cube3}
-    cubeList[3] = {order: parseInt(cube4.style.order), cube: cube4}
-    cubeList[4] = {order: parseInt(cube5.style.order), cube: cube5}
-    cubeList[5] = {order: parseInt(cube6.style.order), cube: cube6}
+// function moveCube (cube,direction) {
+//     const cubeList = []
+//     cubeList[0] = {order: parseInt(cube1.style.order), cube: cube1}
+//     cubeList[1] = {order: parseInt(cube2.style.order), cube: cube2}
+//     cubeList[2] = {order: parseInt(cube3.style.order), cube: cube3}
+//     cubeList[3] = {order: parseInt(cube4.style.order), cube: cube4}
+//     cubeList[4] = {order: parseInt(cube5.style.order), cube: cube5}
+//     cubeList[5] = {order: parseInt(cube6.style.order), cube: cube6}
     
-    console.log (cubeList)
-    cubeList.sort (function(a,b) {
-        //console.log (a.order-b.order)
-        return a.order-b.order
-    })
-    console.log (cubeList)
+//     cubeList.sort (function(a,b) {
+//         return a.order-b.order
+//     })
 
-    const index = cubeList.findIndex (function (cubes) {
-        return cubes.cube === cube
-    })
+//     const index = cubeList.findIndex (function (cubes) {
+//         return cubes.cube === cube
+//     })
     
-    if (direction === 'right')
-        if (index < cubeList.length-1) {
-            buf = cubeList[index]
-            cubeList[index] = cubeList[index+1]
-            cubeList[index+1] = buf
+//     if (direction === 'right') {
+//         if (index < cubeList.length-1) {
+//             buf = cubeList[index]
+//             cubeList[index] = cubeList[index+1]
+//             cubeList[index+1] = buf
 
-            cubeList.forEach (function(item, i, arr) {
-                item.cube.style.order = i
-            })
-        }
-    else if (direction === 'left')
-        console.log(index)
-        if (index > 0) {
-            buf = cubeList[index]
-            cubeList[index] = cubeList[index-1]
-            cubeList[index-1] = buf
+//             cubeList.forEach (function(item, i, arr) {
+//                 item.cube.style.order = i
+//             })
+//         }
+//     }
+//     else if (direction === 'left') {
+//         if (index > 0) {
+//             buf = cubeList[index]
+//             cubeList[index] = cubeList[index-1]
+//             cubeList[index-1] = buf
 
-            cubeList.forEach (function(item, i, arr) {
-                item.cube.style.order = i
-            })
-        }
+//             cubeList.forEach (function(item, i, arr) {
+//                 item.cube.style.order = i
+//             })
+//         }
+//     }
+// }
 
-}
 
-
-cube1.addEventListener ('click', (event) => {
-    event.preventDefault()
-    //moveCube (cube1,'right')
-    const cubeList = []
-    cubeList[0] = {order: parseInt(cube1.style.order), cube: cube1}
-    cubeList[1] = {order: parseInt(cube2.style.order), cube: cube2}
-    cubeList[2] = {order: parseInt(cube3.style.order), cube: cube3}
-    cubeList[3] = {order: parseInt(cube4.style.order), cube: cube4}
-    cubeList[4] = {order: parseInt(cube5.style.order), cube: cube5}
-    cubeList[5] = {order: parseInt(cube6.style.order), cube: cube6}
+// cube1.addEventListener ('click', (event) => {
+//     event.preventDefault()
+//     moveCube (cube1,'right')
+    // const cubeList = []
+    // cubeList[0] = {order: parseInt(cube1.style.order), cube: cube1}
+    // cubeList[1] = {order: parseInt(cube2.style.order), cube: cube2}
+    // cubeList[2] = {order: parseInt(cube3.style.order), cube: cube3}
+    // cubeList[3] = {order: parseInt(cube4.style.order), cube: cube4}
+    // cubeList[4] = {order: parseInt(cube5.style.order), cube: cube5}
+    // cubeList[5] = {order: parseInt(cube6.style.order), cube: cube6}
     
-    console.log(cubeList)
-    cubeList.sort(function(a,b) {
-        return a.order-b.order
-    })
-    console.log(cubeList)
+    // console.log(cubeList)
+    // cubeList.sort(function(a,b) {
+    //     return a.order-b.order
+    // })
+    // console.log(cubeList)
 
-    const index = cubeList.findIndex (function (cubes) {
-        return cubes.cube === cube1
-    })
+    // const index = cubeList.findIndex (function (cubes) {
+    //     return cubes.cube === cube1
+    // })
     
-    if (index < cubeList.length-1) {
-    buf = cubeList[index]
-    cubeList[index] = cubeList[index+1]
-    cubeList[index+1] = buf
+    // if (index < cubeList.length-1) {
+    // buf = cubeList[index]
+    // cubeList[index] = cubeList[index+1]
+    // cubeList[index+1] = buf
 
-    cubeList.forEach (function(item, i, arr) {
-        //console.log(item.cube)
-        item.cube.style.order = i
-    })
-    }
-    cubeList.length = 0
-})
-
-cube1.addEventListener ('contextmenu', (event) => {
-    event.preventDefault()
-    //moveCube (cube1,'left')
-})*/
+    // cubeList.forEach (function(item, i, arr) {
+    //     //console.log(item.cube)
+    //     item.cube.style.order = i
+    // })
+    // }
+    // cubeList.length = 0
+//})
